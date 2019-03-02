8c5777ac7376f56249c346a7d32d1dda2231e2d2
skbhagat40 <skbhagat40@gmail.com>
2019-03-02 13:31:17 +0530

    a

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedict
 from django.views import generic
 from .models import Tasks
 from django.urls import reverse,reverse_lazy
