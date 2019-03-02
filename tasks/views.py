703c1a2ea9026edba911b9d93a5af8573873a804
skbhagat40 <skbhagat40@gmail.com>
2019-03-02 13:34:49 +0530

    error fix

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedict
+from django.http import HttpResponse,HttpResponseRedirect
 from django.views import generic
 from .models import Tasks
 from django.urls import reverse,reverse_lazy
